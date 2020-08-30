1be4b91ac9bb0354aae2916832c6aa59c66f3339
vishal bhardwaj <infovishalbhardwaj232001@gmail.com>
2020-08-30 21:50:42 +0530

    coming-soon

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ mongoose
   });
 
 var app = express();
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
